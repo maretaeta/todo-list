5dbbd85aa590fcdc6f1ee9bf4e57594d738bf24b
unknown <maretanur197@gmail.com>
2023-03-31 22:31:30 +0700

    update button

--- src/components/DeleteTodo.jsx
+++ src/components/DeleteTodo.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const DeleteTodo = ({clearList}) => {
   return (
-    <div className='text-center justify-center pt-7 flex'> 
+    <div className='text-center justify-center pt-7 flex gap-8 md:gap-10 lg:gap-16'> 
          <button
          onClick={clearList}
             type="submit"
-            class=" ml-2 text-sm text-center text-white bg-red-600 rounded-sm w-44 h-8"
+            class=" ml-2 text-sm text-center text-white bg-red-600 rounded-sm w-44 md:w-60 lg:w-64 h-8"
           >
             Delete All Task
           </button>
@@ -14,7 +14,7 @@ const DeleteTodo = ({clearList}) => {
           <button
           
             type="submit"
-            class=" ml-2 text-sm text-center text-white bg-red-600 rounded-sm w-44 h-8"
+            class=" ml-2 text-sm text-center text-white bg-red-600 rounded-sm w-44 md:w-60 lg:w-64 h-8"
           >
             Delete Done Task
           </button>
